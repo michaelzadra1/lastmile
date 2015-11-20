464b4979c7782c67c88f663221072635082e3578
Michael Zadra <michaelzadra1@gmail.com>
2015-11-20 12:30:14 -0500

    Last Mile website cover

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -1,5 +1,11 @@
 myApp.controller('HomeController', function($scope) {
 	// Set animation class
 	$scope.pageClass = 'home';
-
+	
+	
+	// Carousel Slider
+	$scope.myInterval = 5000;
+	$scope.noWrapSlides = false;
+	$scope.slides = ["/images/image1.jpg", "/images/image2.png", "/images/image3.png"];
+	
 }); //MainController
